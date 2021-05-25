1e3c41741cdc1ad0e5b75a4f54fc885bb904ed38
Sylvain Afchain <safchain@gmail.com>
2021-05-25 09:45:21 +0200

    Add service tag

--- pkg/security/module/server.go
+++ pkg/security/module/server.go
@@ -147,10 +147,13 @@ func (a *APIServer) start(ctx context.Context) {
 					msg.tags[tag] = true
 				}
 
+				// recopy tags
 				var tags []string
 				for tag := range msg.tags {
 					tags = append(tags, tag)
 				}
+
+				fmt.Printf("TAGS: %+v\n", tags)
 
 				m := &api.SecurityEventMessage{
 					RuleID: msg.ruleID,
